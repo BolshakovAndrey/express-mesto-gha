7969e8b20893ba010f072b1682339914039d6b23
BolshakovAndrey <abolshakovy@gmail.com>
2022-06-08 17:06:35 +0300

    Feature(hotfix) Update app.js

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ const StatusCodes = require('./utils/utils');
 // подключаемся к серверу mongo
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
   useUnifiedTopology: true,
+  // useCreateIndex: true,
+  // useFindAndModify: false,
 });
 
 const { PORT = 3000 } = process.env;
