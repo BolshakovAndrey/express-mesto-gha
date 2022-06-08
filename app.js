49d0ff3a2ddaf5976d5f7355e64549a19313b8d2
BolshakovAndrey <abolshakovy@gmail.com>
2022-06-08 16:32:18 +0300

    Feature(hotfix) Create Delete Card

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const StatusCodes = require('./utils/utils');
 // подключаемся к серверу mongo
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
-  // useCreateIndex: true,
-  // useFindAndModify: false,
+  useCreateIndex: true,
+  useFindAndModify: false,
   useUnifiedTopology: true,
 });
 
